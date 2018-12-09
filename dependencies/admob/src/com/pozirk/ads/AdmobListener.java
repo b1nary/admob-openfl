17aca53c9091c2f89fdc20863aba7811c9ac74fe
Roman Pramberger <b1nary@users.noreply.github.com>
2018-12-09 17:34:50 +0100

    This might just work....

--- dependencies/admob/src/com/pozirk/ads/AdmobListener.java
+++ dependencies/admob/src/com/pozirk/ads/AdmobListener.java
@@ -95,4 +95,46 @@ public class AdmobListener extends AdListener
    {
 		 _callback.call("onStatus", new Object[] {_who+"_LEFT_APP", ""});
    }
+   
+@Override
+public void onRewarded(RewardItem reward) {
+    _callback.call("onStatus", new Object[] {_who+"_ON_REWARDED", ""});
+}
+
+@Override
+public void onRewardedVideoAdLeftApplication() {
+    _callback.call("onStatus", new Object[] {_who+"_ON_REWARDED_VIDEO_AD_LEFT_APPLICATION", ""});
+
+}
+
+@Override
+public void onRewardedVideoAdClosed() {
+    _callback.call("onStatus", new Object[] {_who+"_ON_REWARDED_VIDEO_AD_CLOSED", ""});
+}
+
+@Override
+public void onRewardedVideoAdFailedToLoad(int errorCode) {
+    _callback.call("onStatus", new Object[] {_who+"_ON_REWARDED_VIDEO_AD_FAILED_TO_LOAD", ""});
+}
+
+@Override
+public void onRewardedVideoAdLoaded() {
+    _callback.call("onStatus", new Object[] {_who+"_ON_REWARDED_VIDEO_AD_LOADED", ""});
+}
+
+@Override
+public void onRewardedVideoAdOpened() {
+    _callback.call("onStatus", new Object[] {_who+"_ON_REWARDED_VIDEO_AD_OPENED", ""});
+}
+
+@Override
+public void onRewardedVideoStarted() {
+    _callback.call("onStatus", new Object[] {_who+"_ON_REWARDED_VIDEO_STARTED", ""});
+}
+
+@Override
+public void onRewardedVideoCompleted() {
+    _callback.call("onStatus", new Object[] {_who+"_ON_REWARDED_VIDEO_COMPLETED", ""});
+}
+   
 }
